3c509ead5f1ccbdcdbe3c0c7d45450d1d99a2bac
Eugene Shereverov <eshereverov@gmail.com>
2018-11-03 23:13:49 +0300

    right slider complit

--- src/js/common/works-slider.js
+++ src/js/common/works-slider.js
@@ -1,82 +1,140 @@
 // buttons slider
 
-let sliderStart = new Slider();
-sliderStart.init();
+let sliderStart = new Slider({
+  containerView: '.slider__views-list',
+  containerLeft: '.buttons-left__list',
+  containerRight: '.buttons-right__list',
+  slide: '.slide',
+  buttonsLeft: '.slider__buttons-left .slider__buttons-img',
+  buttonsRight: '.slider__buttons-right .slider__buttons-img',
+  duration: '700',
 
+});
 
-
-
-function Slider() {
+function Slider(options) {
   let that = this;
   that.counter = 0;
+  that.counterPrev;
+  that.counterNext;
   that.inProcess = false;
-  that.containerOne = document.querySelector('.slider__buttons-left');
-  that.containerTwo = document.querySelector('.slider__buttons-right');
-  that.buttonsLeft = that.containerOne.querySelector('.slider__buttons-img');
-  that.buttonsRight = that.containerTwo.querySelector('.slider__buttons-img');
-  that.duration = 400;
+  that.containerView = document.querySelector(options.containerView);
+  that.containerLeft = document.querySelector(options.containerLeft);
+  that.containerRight = document.querySelector(options.containerRight);
+  that.lenghtChild = Math.max(
+    Array.from(that.containerView.querySelectorAll(options.slide)).length,
+    Array.from(that.containerLeft.querySelectorAll(options.slide)).length,
+    Array.from(that.containerRight.querySelectorAll(options.slide)).length
+  );
+  that.buttonsLeft = document.querySelector(options.buttonsLeft);
+  that.buttonsRight = document.querySelector(options.buttonsRight);
+  that.duration = options.duration;
+  
+  that.moveSlide = function(container, orientation, direction) {
 
-  that.moveSlide = function(container) {
-    let items = Array.from(container.querySelectorAll('.slide'));
-    let [activeItem] = items.filter((el)=> el.classList.contains('active'));
+    let items = Array.from(container.children);
+    let activeItem = {};
+    let reqItem;
+    
+    items.forEach((el, i)=> {
+      if(el.classList.contains('active')) {
+        activeItem.elem = el,
+        activeItem.index = i
+      }
+    });
+
     if(that.counter >= items.length) that.counter = 0;
     if(that.counter < 0) that.counter = items.length - 1;
-    let reqItem = items[that.counter];
 
-    animate({
-      duration: that.duration,
-      draw(progress) {
-        activeItem.style.top = `${progress}%`;
-      }
-    });
+    that.countSlide( activeItem.index, that.lenghtChild);
+    activeItem = activeItem.elem;
+    if(orientation === 'prev') {
+      reqItem = items[that.counterPrev];
+    }
 
-    animate({
-      duration: that.duration,
-      draw(progress) {
-        reqItem.style.top = `${-100 + progress}%`;
-      },
-      after() {
-        activeItem.classList.remove('active');
-        reqItem.classList.add('active');
-        that.inProcess = false;
-      }
-    });
+    if(orientation === 'next') {
+      reqItem = items[that.counterNext];
+    }
 
+    if(!direction) {
+      activeItem.classList.remove('active');
+      reqItem.classList.add('active');
+    } else {
+      animate({
+        duration: that.duration,
+        draw(progress) {
+          activeItem.style.top = (direction === 'down') ? `${progress}%` : `${-progress}%`;
+        }
+      });
+  
+      animate({
+        duration: that.duration,
+        draw(progress) {
+          reqItem.style.top = (direction === 'down') ? `${(-100) + progress}%` : `${100 - progress}%`;
+        },
+        after() {
+          activeItem.classList.remove('active');
+          reqItem.classList.add('active');
+          that.inProcess = false;
+        }
+      });
+    }
   }
 
   that.init = function() {
+    // calculation counters
+    that.countSlide(that.counter, that.lenghtChild);
+    // calculation counters
 
+    // initial first slides
+    that.slideInit(that.containerView, that.counter);
+    that.slideInit(that.containerLeft, that.counterPrev);
+    that.slideInit(that.containerRight, that.counterNext);
+    // initial first slides
+
+    // listeners
     that.buttonsLeft.addEventListener('click', ()=> {
-
       if(!that.inProcess) {
-        that.inProcess = true
-        that.moveSlide(that.containerOne);
-        that.counter--;
+        that.inProcess = true;
+        that.moveSlide(that.containerView, 'prev');
+        that.moveSlide(that.containerLeft,  'prev', 'down');
+        that.moveSlide(that.containerRight,  'prev', 'up');
+        // that.counter--;
       }
-    })
-
+    });
     that.buttonsRight.addEventListener('click', ()=> {
-
       if(!that.inProcess) {
-        that.inProcess = true
-        that.moveSlide(that.containerTwo);
-        that.counter++;
+        that.inProcess = true;
+        that.moveSlide(that.containerView, 'next');
+        that.moveSlide(that.containerLeft, 'next', 'down');
+        that.moveSlide(that.containerRight, 'next', 'up');
+        // that.counter++;
       }
-    })
-
+    });
+    // listeners
   }
+
+  that.slideInit = (container, i)=> {
+    let child = container.children;
+    child[i].classList.add('active');
+  }
+  
+  that.countSlide = (counter, arrSlide)=> {
+    if(counter === arrSlide - 1) {
+      that.counterNext = 0
+    } else {
+      that.counterNext = counter + 1
+    }
+    if(counter === 0) {
+      that.counterPrev = arrSlide - 1
+    } else {
+      that.counterPrev = counter - 1
+    }
+  }
+
+  that.init();
 }
 
-
-
 // ===  RAF  ===
-// duration – длительность анимации в мс.
-// timing – функция, которая определяет состояние анимации каждый кадр. Получает часть времени от 0 до 1, возвращает завершенность анимации от 0 до 1.
-// draw – функция, которая отрисовывает состояние анимации от 0 до 1.
-
-// let duration = 1000;
-
-
 function animate({draw, duration, after}) {
 
   let start = performance.now();
@@ -97,112 +155,3 @@ function animate({draw, duration, after}) {
     }
   });
 }
-
-
-// animate({
-//   duration: duration,
-//   timing: function timing(timeFraction) {
-//     // return timeFraction;
-//     return timeFraction
-//   },
-//   draw: function(progress) {
-    
-//   }
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const sliderViewsItems = Array.prototype.slice.call(document.querySelectorAll('.slider__views .slider__views-item'));
-// const sliderBtnleftItems = Array.prototype.slice.call(document.querySelectorAll('.slider__buttons-left .buttons-left__item'));
-// const sliderBtnRightItems = Array.prototype.slice.call(document.querySelectorAll('.slider__buttons-right .buttons-right__item'));
-
-// let counter = 0;
-// let counterNext, counterPrev;
-
-// if(sliderViewsItems.length && sliderBtnleftItems.length && sliderBtnRightItems.length) {
-
-//   const buttonPrev = document.querySelector('.slider__buttons-left .slider__buttons-img');
-//   const buttonNext = document.querySelector('.slider__buttons-right .slider__buttons-img');
-
-//   countSlide(counter, sliderViewsItems)
-//   sliderViewsItems[counter].classList.add('active')
-//   sliderBtnleftItems[counterPrev].classList.add('active')
-//   sliderBtnRightItems[counterNext].classList.add('active')
-
-//   console.log(counterPrev, counter, counterNext);
-
-
-//   buttonNext.addEventListener('click', function() {
-//     counter++
-//     coreSlider(sliderViewsItems, sliderBtnleftItems, sliderBtnRightItems, 'up')
-//   })
-  
-//   buttonPrev.addEventListener('click', function() {
-//     counter--
-//     coreSlider(sliderViewsItems, sliderBtnleftItems, sliderBtnRightItems, 'down')
-//   })
-// }
-
-// function coreSlider(arrSlide, arrPrevSlide, arrNextSlide, direction) {
-//   countSlide(counter, arrSlide)
-
-//   if(counter > (arrSlide.length - 1)) {
-//     counter = 0;
-//   }
-
-//   if(counter < 0) {
-//     counter = (arrSlide.length - 1);
-//   }
-
-//   if(direction === 'up') {
-//     arrSlide[counterPrev].classList.remove('active')
-//   }
-//   if(direction === 'down') {
-//     arrSlide[counterNext].classList.remove('active')
-//   }
-//   arrSlide[counter].classList.add('active')
-
-  
-// }
-
-// function countSlide(counter, arrSlide) {
-//   if(counter === arrSlide.length - 1) {
-//     counterNext = 0
-//   } else {
-//     counterNext = counter + 1
-//   }
-//   if(counter === 0) {
-//     counterPrev = arrSlide.length - 1
-//   } else {
-//     counterPrev = counter - 1
-//   }
-// }
